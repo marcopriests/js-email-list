062431d1281a77d3e65aa7a90d65061f43b698b4
marcopriests <prtmrc98@gmail.com>
2025-04-04 10:35:46 +0200

    [BONUS] ho modificato gran parte del codice a finché funzionasse il bottone "generate again"

--- js/script.js
+++ js/script.js
@@ -1,31 +1,35 @@
-// creo la funzione in cui creerò l'elemento in cui andrò a salvare la mail generata
-// aggiungo un parametro numerico
-const generateMails = (num) => {
-    // recupero l'elemento dal dom
-    const lista = document.getElementById('mail-list')
-
-    // sposto la chiamata ajax all'interno della funzione
-
-    // creo un ciclo che effettui la chiamata ajax per tante volte quante inserite nella variabile mailNum
-    for (let i = 0; i <= num; i++) {
-
-        // effettuo la chiamata ajax
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) => {
-
-            // creo l'elemento 
-            const li = document.createElement('li')
-
-            // appendo la mail generata all'elemento creato
-            li.append(resp.data.response)
-
-            // appendo il nuovo elemento creato alla lista come figlio
-            lista.appendChild(li)
-        })
-    }
-}
+// recupero l'elemento dal dom
+const lista = document.getElementById('mail-list')
 
 // creo una variabile numerica per far generare n mail
 const mailNum = 10
 
+// creo la funzione in cui creerò l'elemento in cui andrò a salvare la mail generata
+const generateMails = (list, num) => {
+
+    // creo un ciclo che effettui la chiamata ajax per tante volte quante inserite nella variabile mailNum
+    for (let i = 0; i < num; i++) {
+
+        // inizializzo il parametro list vuoto in modo che si azzeri ogni volta che viene richiamata la funzione
+        list.innerHTML = ''
+
+        // effettuo la chiamata ajax
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) => {
+
+            // appendo la mail generata all'elemento creato
+            list.innerHTML += `<li>${resp.data.response}</li>`
+        })
+    }
+}
+
 // richiamo la funzione
-generateMails(mailNum)
+generateMails(lista, mailNum)
+
+// recupero il bottone dal dom
+const button = document.querySelector('.btn')
+
+// richiamo la funzione al click del bottone
+button.addEventListener('click', () =>{
+    generateMails(lista, mailNum)
+})
+
